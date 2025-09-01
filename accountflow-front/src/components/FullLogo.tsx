8bd43c622bfbf53d2dc0da159989cfaf526f1cbd
Vitor Dallanol <vitor.dallanol@gmail.com>
2025-09-01 20:07:30 -0300

    fix: lint

--- accountflow-front/src/components/FullLogo.tsx
+++ accountflow-front/src/components/FullLogo.tsx
@@ -1,15 +1,12 @@
 import Logo from '@/assets/images/logos/logoTransparente2.png';
 const FullLogo = ({ withoutLink }: { withoutLink?: boolean }) => {
-
   if (withoutLink) {
-    return (
-      <img src={Logo.src} alt='logo' className='block' />
-    );
+    return <img src={Logo.src} alt="logo" className="block" />;
   }
 
   return (
     <a href={'/'}>
-      <img src={Logo.src} alt='logo' className='block' />
+      <img src={Logo.src} alt="logo" className="block" />
     </a>
   );
 };
