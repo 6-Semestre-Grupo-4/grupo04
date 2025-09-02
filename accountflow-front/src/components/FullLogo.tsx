b9580a60f984b0d6710004c27be7a41f24aedd1c
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2025-09-01 21:17:17 -0300

    Feature/new init (#12)
    * feat: começando com a nova tecnologia adotada, django com vite + react routes
    
    * feat: login page
    
    * fix: lint
    
    * fix: lint

---
+++ accountflow-front/src/components/FullLogo.tsx
@@ -0,0 +1,14 @@
+import Logo from '@/assets/images/logos/logoTransparente2.png';
+const FullLogo = ({ withoutLink }: { withoutLink?: boolean }) => {
+  if (withoutLink) {
+    return <img src={Logo.src} alt="logo" className="block" />;
+  }
+
+  return (
+    <a href={'/'}>
+      <img src={Logo.src} alt="logo" className="block" />
+    </a>
+  );
+};
+
+export default FullLogo;
